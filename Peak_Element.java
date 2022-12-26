91ccf823d8901655daa96ac10ce67e5d4d50b5b6
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-26 14:41:30 +0530

    Update Peak_Element.java

--- Peak_Element.java
+++ Peak_Element.java
@@ -1,21 +1,60 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
 
 
+import java.util.*;
+import java.io.*;
+class PeakElement{
+	public static void main(String[] args)throws IOException{
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] a=new int[n];
+		    int[] tmp=new int[n];
+		    String s[]=in.readLine().trim().split(" ");
+			for(int i=0;i<n;i++){
+				a[i]=Integer.parseInt(s[i]);
+				tmp[i] = a[i];
+			}
+		    
+		    int f=0;
+			int A=(new Solution()).peakElement(tmp,n);
+			
+			if(A<0 && A>=n)
+			    out.println(0);
+			else
+			{
+    			if(n==1 && A==0)
+    			    f=1;
+    			else if(A==0 && a[0]>=a[1])
+    			    f=1;
+    			else if(A==n-1 && a[n-1]>=a[n-2])
+    			    f=1;
+    			else if(A >= 0 && A < n && a[A] >=a[A+1] && a[A]>= a[A-1])
+    			    f=1;
+    			else
+    			    f=0;
+			    out.println(f);
+			}
+		}
+		out.close();
+	}
+}
 // } Driver Code Ends
-/* The function should return the index of any
-   peak element present in the array */
 
-// arr: input array
-// n: size of array
+
+/*Complete the function below*/
+
 class Solution
 {
-    public:
-    int peakElement(int arr[], int n)
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
     {
-       // Your code here
-       int max = arr[0];
+       //add code here.
+        int max = arr[0];
        int index = 0;
        for(int i=1;i<n;i++){
            if(arr[i]>max){
@@ -25,47 +64,4 @@ class Solution
        }
        return index;
     }
-};
-
-//{ Driver Code Starts.
-
-int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int n;
-		cin>>n;
-		int a[n], tmp[n];
-		for(int i=0;i<n;i++)
-		{
-			cin>>a[i];
-			tmp[i] = a[i];
-		}
-		bool f=0;
-		Solution ob;
-		
-		int A = ob. peakElement(tmp,n);
-		
-		if(A<0 and A>=n)
-		    cout<<0<<endl;
-		else
-		{
-    		if(n==1 and A==0)
-    		    f=1;
-    		else if(A==0 and a[0]>=a[1])
-    		    f=1;
-    		else if(A==n-1 and a[n-1]>=a[n-2])
-    		    f=1;
-    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
-    		    f=1;
-    		else
-    		    f=0;
-    		cout<<f<<endl;
-		}
-		
-	}
-
-	return 0;
 }
-// } Driver Code Ends
