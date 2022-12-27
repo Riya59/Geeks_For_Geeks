e110b97681799b51ce0820110bd46d6bb3532244
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-27 17:05:43 +0530

    Create duplicate_in_array.java

---
+++ duplicate_in_array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            Solution g = new Solution();
+            ArrayList<Integer> ans = g.duplicates(a, n);
+            for (Integer val : ans) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        // code here
+        ArrayList<Integer>list = new ArrayList<>();
+        Arrays.sort(arr);
+        int count = 0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i] == arr[i+1] && count!=1){
+                list.add(arr[i]);
+                count++;
+            }
+            else if(arr[i] != arr[i+1]){
+                count = 0;
+            }
+        }
+        if(list.size() == 0){
+            list.add(-1);
+        }
+        return list;
+     
+    }
+}
